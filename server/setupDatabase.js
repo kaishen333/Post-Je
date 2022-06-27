4ec7d2cddd3cf97a7812a7a297c4238d777e451d
Lim Kai Shen <70677197+kaishen333@users.noreply.github.com>
2022-06-27 23:59:52 +0800

    db schema

--- server/setupDatabase.js
+++ server/setupDatabase.js
@@ -70,6 +70,50 @@ const { DB } = require('./config');
       FOREIGN KEY (productId) REFERENCES products(id)
     );
   `
+  const stateTableStmt = `
+    CREATE TABLE IF NOT EXISTS state (
+      id              INT             PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
+      name            VARCHAR(50)     NOT NULL,
+    );
+  `
+  const postcodeTableStmt = `
+    CREATE TABLE IF NOT EXISTS postcode (
+      id              INT             PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
+      stateId         INT             NOT NULL,
+      postcode        INT             NOT NULL,
+      FOREIGN KEY (stateId) REFERENCES state(id),
+    );
+  `
+  const dropOffTableStmt = `
+    CREATE TABLE IF NOT EXISTS dropOff (
+      id              INT             PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
+      postcodeId      INT             NOT NULL,
+      courierId       INT             NOT NULL,
+      lat             INT             NOT NULL,
+      long            INT             NOT NULL,
+      FOREIGN KEY (postcodeId) REFERENCES postcode(id),
+      FOREIGN KEY (courierId) REFERENCES courier(id)
+    );
+  `
+  const courierTableStmt = `
+    CREATE TABLE IF NOT EXISTS courier (
+      id              INT             PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
+      name            VARCHAR(50)     NOT NULL,
+    );
+  `
+  const packageTableStmt = `
+    CREATE TABLE IF NOT EXISTS package (
+      id              INT             PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
+      courierId       INT             NOT NULL,
+      length          INT             NOT NULL,
+      width           INT             NOT NULL,
+      height          INT             NOT NULL,
+      price           INT             NOT NULL,
+      weight          INT             NOT NULL,
+      link            VARCHAR(100)    NOT NULL,
+      FOREIGN KEY (courierId) REFERENCES courier(id),
+    );
+  `
 
   try {
     const db = new Client({
