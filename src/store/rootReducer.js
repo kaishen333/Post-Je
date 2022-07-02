a8a454848c89a73e7e3066fd3fd0039e61e8eb20
Lim Kai Shen <70677197+kaishen333@users.noreply.github.com>
2022-07-02 17:38:08 +0800

    src store

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,4 +1,10 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from "redux";
+import packageReducer from "./package/Package.reducer";
+import courierReducer from "./courier/Courier.reducer";
+import googleMapsReducer from "./googleMaps/GoogleMaps.reducer";
 
 export default combineReducers({
+  package: packageReducer,
+  courierReducer: courierReducer,
+  googleMaps: googleMapsReducer,
 });
